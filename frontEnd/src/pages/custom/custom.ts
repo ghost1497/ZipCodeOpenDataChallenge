4ca3eac52cfd9df28c15f5bf3078e30208ade26c
ghost1497 <rfrankie04@gmail.com>
2018-05-04 17:46:00 -0400

    itinerary looking nice

--- frontEnd/src/pages/custom/custom.ts
+++ frontEnd/src/pages/custom/custom.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { NplistPage } from '../nplist/nplist';
-import { ItineraryPage } from '../itinerary/itinerary';
-import { Storage } from '@ionic/storage';
-import { AlertController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { NplistPage } from "../nplist/nplist";
+import { ItineraryPage } from "../itinerary/itinerary";
+import { Storage } from "@ionic/storage";
+import { AlertController } from "ionic-angular";
 
 /**
  * Generated class for the CustomPage page.
@@ -14,37 +14,42 @@ import { AlertController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-custom',
-  templateUrl: 'custom.html',
+  selector: "page-custom",
+  templateUrl: "custom.html"
 })
 export class CustomPage {
-
   itinerary = [];
-  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage, public alertCtrl: AlertController) {
-  }
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public storage: Storage,
+    public alertCtrl: AlertController
+  ) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad CustomPage');
+    console.log("ionViewDidLoad CustomPage");
   }
 
-  toNationalParkList(){
+  toNationalParkList() {
     this.navCtrl.push(NplistPage);
   }
 
-  toShoppingCart(){
+  toShoppingCart() {
     this.storage.keys().then(data => {
       this.itinerary = data;
     });
     console.log(this.itinerary);
-    if(this.itinerary.length == 0){
+    if (this.itinerary.length == 0) {
       let alert = this.alertCtrl.create({
-        title: 'Empty Itinerary',
-        subTitle: 'Your itinerary seems to be empty. Try adding a location by clicking on one of our sections!',
-        buttons: ['OK']
+        title: "Empty Itinerary",
+        subTitle:
+          "Your itinerary seems to be empty. Try adding a location by clicking on one of our sections!",
+        buttons: ["OK"]
       });
       alert.present();
       return;
-      }
-    this.navCtrl.push(ItineraryPage);
+    } else {
+      this.navCtrl.push(ItineraryPage);
+    }
   }
 }
